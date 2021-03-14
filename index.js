3a2025ac05f748a5852e18872e7c0a80ad7ba775
master-tecs <71886059+master-tecs@users.noreply.github.com>
2021-03-14 16:03:07 +0300

    First commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ if (!config.has("jwtPrivateKey")) {
 }
 
 mongoose
-  .connect("mongodb://localhost/todo")
+  .connect(process.env.PORT || "mongodb://localhost/todo")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB..."));
 
